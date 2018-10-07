320ba5c721e28851bbc5e7004ca58962de147583
James Nighswonger <james.nighswonger@gmail.com>
2018-10-07 13:43:52 -0700

    Added complete tests for GET and POST

--- test/test.js
+++ test/test.js
@@ -15,9 +15,32 @@ describe("Blog Posts", function () {
         return closeServer();
     });
 
-    it ("should list posts on GET", function () {
-        return chai.request(app).get("/blog-posts").then (function (res) {
+    it("should list posts on GET", function () {
+        return chai.request(app).get("/blog-posts").then(function (res) {
             expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a("array");
+            expect(res.body.lenth).to.be.at.least(1);
+
+            const expectedKeys = ['title', 'content', 'author'];
+            res.body.forEach(function (item) {
+                expect(item).to.be.a("object");
+                expect(item).to.include.keys(expectedKeys);
+            });
+        });
+    });
+
+    it("should add a blog post on POST", function () {
+        const newPost = { title: "New Title", content: "New content", author: "New Author" };
+        return chai.request(app).post("/blog-posts").send(newPost).then(function (res) {
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a("object");
+            expect(res.body).to.include.keys("title", "content", "author");
+            expect(res.body.id).to.not.equal(null);
+            expect(res.body).to.deep.equal(
+                Object.assign(newItem, { id: res.body.id })
+            );
         });
     });
 });
